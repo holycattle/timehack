0b75e212a901214e27a8359323f7c316dfdb5ca4
Joshua Castaneda <thejosh223@gmail.com>
2013-08-24 15:19:09 +0800

    Initial Commit.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 6;
+
+	void Awake() {
+	}
+
+	void Start() {
+	}
+
+	void Update() {
+		float xAxis = Input.GetAxis("Horizontal");
+		float zAxis = Input.GetAxis("Vertical");
+
+		if (Mathf.Abs(xAxis) != 0) {
+			Debug.Log("X Movement");
+		}
+
+		if (Mathf.Abs(zAxis) != 0) {
+			Debug.Log("Y Movement");
+		}
+
+		transform.Translate(new Vector3(xAxis * moveSpeed * Time.deltaTime, 0, zAxis * moveSpeed * Time.deltaTime));
+	}
+}
