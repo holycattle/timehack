e27525226313819d59bdc82d053913fa3e8fbd17
Joshua Castaneda <thejosh223@gmail.com>
2013-08-25 07:14:09 +0800

    Changed Background.

--- Assets/Scripts/Mobs/MobController.cs
+++ Assets/Scripts/Mobs/MobController.cs
@@ -6,14 +6,14 @@ public class MobController : MonoBehaviour {
 	public float rotationSpeed = 180;
 	private PlayerController player;
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		//TODO: initialize random stats here
 		moveSpeed = 1f;
 		player = PlayerController.getPlayerControllerInstance();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		transform.LookAt(player.transform.position);
 		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.Self);
 	}
